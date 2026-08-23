ab455b3160d54d170c8dd2cb852daaa04dbce9b4
abhnvgrg <abhinavdev.co@gmail.com>
2026-08-24 00:08:58 +0530

    db schema

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,19 @@
+from app.models.entities import (
+    AuditLedger,
+    Case,
+    CaseAssignment,
+    Document,
+    DocumentAccessGrant,
+    PhysicalAsset,
+    User,
+)
+
+__all__ = [
+    "AuditLedger",
+    "Case",
+    "CaseAssignment",
+    "Document",
+    "DocumentAccessGrant",
+    "PhysicalAsset",
+    "User",
+]
